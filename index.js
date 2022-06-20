649a9331e1c48a8cbb91d3810cf3c2aed51ee329
prts44 <ryan.ckit57@gmail.com>
2022-06-20 15:59:01 -0400

    Use cors and bodyParser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // get all exercises (done)
-// get exercise by name
+// get exercise by name (sort of done)
+//    edit data.js to store the names of all exercises as "ids" (all lowercase and no spaces)
 // get exercises by type
 // get exercises by major muscles
 // get exercises by compound
@@ -8,9 +9,11 @@
 import express from "express";
 const app = express();
 const { PORT = 4000} = process.env;
-//import bodyParser from "body-parser";
-//import cors from "cors";
+import bodyParser from "body-parser";
+import cors from "cors";
 import data from "./exer.js";
+
+app.use(bodyParser.json()).use(cors());
 
 app.get("/api/exercises", (req, res) => {
     res.json(data.exercises);
@@ -25,6 +28,17 @@ app.get("/api/exercises/:name", (req, res) => {
     }
     return res.json(exercise);
 });
+
+app.get("/api/exercises/MajorMuscle/:muscle", (req, res) => {
+    const muscle = req.params.muscle;
+    let exercises = data.exercises; 
+    exercises = exercises.filter((doc) => doc.MajorMuscle.includes(muscle));
+    if (!exercises) {
+        console.log(exercise);
+        res.status(404).json({error: "Could not find any exercises for this muscle"});
+    }
+    return res.json(exercises);
+}); 
 
 app.listen(PORT, () => 
     console.log(`Listening on http://localhost:${PORT}/`)
